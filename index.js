5f5811bd13800049ee360dbbc8ff710e81787031
Thomas Kekeisen <github@tk.ca.kekeisen.it>
2017-05-21 21:24:40 +0200

    Added some initial code and test data

---
+++ index.js
@@ -0,0 +1,35 @@
+//
+const fs = require('fs');
+
+function checkForDataFilePath () {
+    if (process.argv && process.argv[2]) {
+        console.error('Checking configuration: ' + process.argv[2]);
+
+        if (fs.existsSync(process.argv[2])) {
+            return true;
+        } else {
+            console.error('Configuration not found: ' + process.argv[2]);
+        }
+    } else {
+        console.error('Configuration file path not set');
+    }
+
+    return false;
+}
+
+function readDataToObject (filePath) {
+    const content = fs.readFileSync(filePath, 'utf8');
+
+    try {
+        return JSON.parse(content);
+    }
+    catch (exception) {
+        console.error('JSON parsing failed.');
+    }
+}
+
+if (checkForDataFilePath()) {
+    const configuration = readDataToObject(process.argv[2]);
+
+    console.log('GDs', configuration);
+}
